a3e54112bad5f9e523fdaa296ccbb7c3e4dacbc1
L0277 <www.lizabhattarai0277@gmail.com>
2025-06-22 12:51:04 +0545

    Fix Student Model

--- WebApplication3/Models/Student.cs
+++ WebApplication3/Models/Student.cs
@@ -4,6 +4,6 @@
     public string StudentName { get; set; }
     public string StudentEmail { get; set; }
 
-    public string StudentAddress { get; set;
+    public string StudentAddress { get; set; }
 
 }
