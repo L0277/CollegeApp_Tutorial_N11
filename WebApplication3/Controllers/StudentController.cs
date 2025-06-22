4e0732d04e6088d64cf8982ec2ac2446448a886c
L0277 <www.lizabhattarai0277@gmail.com>
2025-06-22 12:45:15 +0545

    Add project files.

---
+++ WebApplication3/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3;
+
+namespace CollegeApp.Controllers
+{
+    [Microsoft.AspNetCore.Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudent()
+        {
+            return CollegeRepository.Students;
+        }
+    }
+}
