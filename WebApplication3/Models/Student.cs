1f5e2d3bf592404e7879ea4afb1fef18a8626f2a
L0277 <www.lizabhattarai0277@gmail.com>
2025-06-22 12:50:47 +0545

    Added Student Model

--- WebApplication3/Models/Student.cs
+++ WebApplication3/Models/Student.cs
@@ -1,1 +1,9 @@
-﻿
+﻿public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public string StudentEmail { get; set; }
+
+    public string StudentAddress { get; set;
+
+}
