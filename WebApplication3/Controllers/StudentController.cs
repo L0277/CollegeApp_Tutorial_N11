699fea88561e9ec26fc57b0181e601bde82a6122
L0277 <www.lizabhattarai0277@gmail.com>
2025-06-22 11:58:39 +0545

    Returning IEnumerable

--- WebApplication3/Controllers/StudentController.cs
+++ WebApplication3/Controllers/StudentController.cs
@@ -8,13 +8,13 @@ namespace WebApplication3.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        public List<Student> GetStudents()
+        public IEnumerable<Student> GetStudents()
         {
             return new List<Student>
             {
                 new Student { StudentID = 1, StudentName = "Ram", StudentAddress = "Kathmandu"  },
                 new Student { StudentID = 2, StudentName = "Sita", StudentAddress = "Bhaktapur"  },
-                new Student { StudentID = 3Re, StudentName = "Shyam", StudentAddress = "Lalitpur"  }
+                new Student { StudentID = 3, StudentName = "Shyam", StudentAddress = "Lalitpur"  }
             };
             
         }
